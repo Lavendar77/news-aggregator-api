db5a071f9210ac28dff6b162c2e42dba0b7ed58c
Adeyinka Adefolurin <folurinyinka@gmail.com>
2025-10-26 00:39:31 +0100

    feat: implement user preference

---
+++ app/Models/UserPreference.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class UserPreference extends Model
+{
+    protected $fillable = [
+        'user_id',
+        'sources',
+        'categories',
+        'authors',
+    ];
+
+    protected $casts = [
+        'sources' => 'array',
+        'categories' => 'array',
+        'authors' => 'array',
+    ];
+
+    /**
+     * Get the user.
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
