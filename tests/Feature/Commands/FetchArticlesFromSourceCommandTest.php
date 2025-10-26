9afd55921e4cbc85bdc7819e98cffc085f5855a0
Adeyinka Adefolurin <folurinyinka@gmail.com>
2025-10-26 17:43:28 +0100

    tests: write more test cases

---
+++ tests/Feature/Commands/FetchArticlesFromSourceCommandTest.php
@@ -0,0 +1,28 @@
+<?php
+
+use App\Console\Commands\FetchArticlesFromSourceCommand;
+use App\Enums\ArticleApiSource;
+use App\Jobs\FetchArticlesFromSourceJob;
+use Illuminate\Support\Facades\Queue;
+
+use function Pest\Laravel\artisan;
+
+it('can dispatch the job for the article source', function () {
+    Queue::fake();
+
+    artisan(FetchArticlesFromSourceCommand::class)->assertSuccessful();
+
+    Queue::assertPushed(FetchArticlesFromSourceJob::class, count(ArticleApiSource::cases()));
+});
+
+
+
+
+it('can dispatch the job for the article source with a specific source', function () {
+    Queue::fake();
+
+    artisan(FetchArticlesFromSourceCommand::class, ['source' => [ArticleApiSource::cases()[0]->value]])
+        ->assertSuccessful();
+
+    Queue::assertPushed(FetchArticlesFromSourceJob::class, 1);
+});
